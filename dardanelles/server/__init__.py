edf67c5716a4ef6124c77925fc19b646c7bf0f5d
Chris Mutel <cmutel@gmail.com>
2022-09-30 12:08:29 +0200

    Specify the template directory

--- dardanelles/server/__init__.py
+++ dardanelles/server/__init__.py
@@ -4,9 +4,10 @@ __version__ = (0, 1)
 
 from flask import Flask
 
-dardanelles_app = Flask("dardanelles")
+template_dir = Path(__file__).parent.resolve() / "templates"
+dardanelles_app = Flask("dardanelles", template_folder=template_dir)
 
-# Default limit for file uploads is 250 MB
-dardanelles_app.config["MAX_CONTENT_LENGTH"] = 250 * 1024 * 1024
+# Default limit for file uploads is 128 MB
+dardanelles_app.config["MAX_CONTENT_LENGTH"] = 128 * 1024 * 1024
 
 from . import app
